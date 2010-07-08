57948b6dd6506d39354dd13bc671aba70052a75b
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2010-07-08 17:35:49 +0700

    Suport for post any form data

--- django_any/tests/test_client_tests.py
+++ django_any/tests/test_client_tests.py
@@ -4,10 +4,36 @@ from django.contrib import admin
 from django.test import TestCase
 from django_any.test import Client
 
+def view(request):
+    """
+    Test view that returning form
+    """
+    from django import forms
+    from django.http import HttpResponse
+    from django.shortcuts import redirect
+    from django.template import Context, Template
+    
+    class TestForm(forms.Form):
+        name = forms.CharField()
+
+    if request.POST:
+        form = TestForm(request.POST)
+        if form.is_valid():
+            return redirect('/view/');
+    else:
+        form = TestForm()
+
+    template = Template("{{ form }}")
+    context = Context({'form' : form})
+
+    return HttpResponse(template.render(context))
+
 
 urlpatterns = patterns('',
      (r'^admin/', include(admin.site.urls)),
+     (r'^view/', view),
 )
+
 
 class TestDjangoAnyClient(TestCase):
     urls = 'django_any.tests.test_client_tests'
@@ -20,4 +46,8 @@ class TestDjangoAnyClient(TestCase):
 
         response = self.client.get('/admin/')
         self.assertEquals(200, response.status_code)
+
+    def test_post_any_data(self):
+        response = self.client.post_any_data('/view/')
+        self.assertRedirects(response, '/view/')
 
