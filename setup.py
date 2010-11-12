963096298105652ad34ff4dd077cf8a96da52405
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2010-11-12 15:16:48 +0600

    Make it installable

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-fsm',
+    version='0.0.1',
+    description='Unobtrusive test models  creation for django.',
+    author='Mikhail Podgurskiy',
+    author_email='kmmbvnr@gmail.com',
+    url='http://github.com/kmmbvnr/django-any',
+    keywords = "django",
+    packages=['django_any', 'django_any.contrib', 'django_any.test'],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT License',
+    platforms = ['any'],
+    classifiers=[
+        'Development Status :: 2 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
