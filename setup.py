792e65db17ea282d3c85e01bcb281b59090300e1
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2011-02-23 11:23:46 +0600

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-any',
-    version='0.1.0',
+    version='0.2.0',
     description='Unobtrusive test models  creation for django.',
     author='Mikhail Podgurskiy',
     author_email='kmmbvnr@gmail.com',
